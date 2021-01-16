18fe6bf051db3cac71ae223fe0bd3162ca720d97
Hansung Kim <stephen422@gmail.com>
2021-01-17 00:38:04 +0900

    Detect exit code in test.sh, make test dir

--- test.sh
+++ test.sh
@@ -1,2 +1,28 @@
 #!/bin/sh
-./ruse test.ruse || exit 1
+
+set -e
+
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+RS="\033[0m"
+
+test() {
+	local ret=0
+	local header=$(head -n1 $1)
+	echo ${header} | grep -q "fail" && ret=1
+	if echo ${header} | grep -q "exit"
+	then
+		ret=$(echo ${header} | awk '{ print $3 }')
+	fi
+
+	./ruse $1
+	if [ $? -ne $ret ]
+	then
+		echo "${RED}FAIL${RS} $1"
+		exit 1
+	else
+		echo "${GREEN}PASS${RS} $1"
+	fi
+}
+
+test test/simple.ruse
