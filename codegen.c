e1c5f40d875cef2768a164ac3313d752ca50ba16
Hansung Kim <stephen422@gmail.com>
2021-08-10 00:49:07 -0700

    Reindent codegen.c and ruse.h

--- codegen.c
+++ codegen.c
@@ -9,103 +9,103 @@
 #include <string.h>
 
 static void emit(const char *fmt, ...) {
-	va_list args;
+    va_list args;
 
-	va_start(args, fmt);
-	vprintf(fmt, args);
-	va_end(args);
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
 }
 
 static int valstack_push_and_incr(struct Context *ctx) {
-	arrput(ctx->valstack.stack, ctx->valstack.curr_id);
-	return ctx->valstack.curr_id++;
+    arrput(ctx->valstack.stack, ctx->valstack.curr_id);
+    return ctx->valstack.curr_id++;
 }
 
 static void codegen_expr(struct Context *ctx, struct Node *n) {
-	char buf[MAXTOKLEN]; // FIXME: stack usage
-	int id_lhs, id_rhs;
+    char buf[MAXTOKLEN]; // FIXME: stack usage
+    int id_lhs, id_rhs;
 
-	tokenstr(ctx->src->src, n->tok, buf, sizeof(buf));
+    tokenstr(ctx->src->src, n->tok, buf, sizeof(buf));
 
-	switch (n->kind) {
-	case ND_LITERAL:
-		emit("	%%_%d =w add 0, %s\n", ctx->valstack.curr_id, buf);
-		valstack_push_and_incr(ctx);
-		break;
-	case ND_IDEXPR:
-		emit("	%%_%d =w add 0, %%%s\n", ctx->valstack.curr_id, buf);
-		valstack_push_and_incr(ctx);
-		break;
-	case ND_BINEXPR:
-		codegen_expr(ctx, n->lhs);
-		codegen_expr(ctx, n->rhs);
+    switch (n->kind) {
+    case ND_LITERAL:
+        emit("    %%_%d =w add 0, %s\n", ctx->valstack.curr_id, buf);
+        valstack_push_and_incr(ctx);
+        break;
+    case ND_IDEXPR:
+        emit("    %%_%d =w add 0, %%%s\n", ctx->valstack.curr_id, buf);
+        valstack_push_and_incr(ctx);
+        break;
+    case ND_BINEXPR:
+        codegen_expr(ctx, n->lhs);
+        codegen_expr(ctx, n->rhs);
 
-		// 'id_rhs' comes first because lhs is pushed to the stack first
-		// during the post-order traversal.
-		id_rhs = arrpop(ctx->valstack.stack);
-		id_lhs = arrpop(ctx->valstack.stack);
-		emit("	%%_%d =w add %%_%d, %%_%d\n", ctx->valstack.curr_id,
-		     id_lhs, id_rhs);
-		valstack_push_and_incr(ctx);
-		break;
-	default:
-		assert(!"unknown expr kind");
-	}
+        // 'id_rhs' comes first because lhs is pushed to the stack first
+        // during the post-order traversal.
+        id_rhs = arrpop(ctx->valstack.stack);
+        id_lhs = arrpop(ctx->valstack.stack);
+        emit("    %%_%d =w add %%_%d, %%_%d\n", ctx->valstack.curr_id,
+             id_lhs, id_rhs);
+        valstack_push_and_incr(ctx);
+        break;
+    default:
+        assert(!"unknown expr kind");
+    }
 }
 
 static void codegen_stmt(struct Context *ctx, struct Node *n) {
-	char buf[MAXTOKLEN];
+    char buf[MAXTOKLEN];
 
-	switch (n->kind) {
-	case ND_EXPRSTMT:
-		codegen(ctx, n->rhs);
-		break;
-	case ND_ASSIGN:
-		codegen(ctx, n->rhs);
-		tokenstr(ctx->src->src, n->lhs->decl->name, buf, sizeof(buf));
-		emit("	%%%s =w add 0, %%_%d\n", buf,
-		     arrpop(ctx->valstack.stack));
-		break;
-	case ND_RETURN:
-		codegen(ctx, n->rhs);
-		emit("	ret %%_%d\n", arrpop(ctx->valstack.stack));
-		break;
-	default:
-		assert(!"unknown stmt kind");
-	}
+    switch (n->kind) {
+    case ND_EXPRSTMT:
+        codegen(ctx, n->rhs);
+        break;
+    case ND_ASSIGN:
+        codegen(ctx, n->rhs);
+        tokenstr(ctx->src->src, n->lhs->decl->name, buf, sizeof(buf));
+        emit("    %%%s =w add 0, %%_%d\n", buf,
+             arrpop(ctx->valstack.stack));
+        break;
+    case ND_RETURN:
+        codegen(ctx, n->rhs);
+        emit("    ret %%_%d\n", arrpop(ctx->valstack.stack));
+        break;
+    default:
+        assert(!"unknown stmt kind");
+    }
 }
 
 void codegen(struct Context *ctx, struct Node *n) {
-	char buf[MAXTOKLEN];
-	int id;
+    char buf[MAXTOKLEN];
+    int id;
 
-	switch (n->kind) {
-	case ND_FILE:
-		emit("export function w $main() {\n");
-		emit("@start\n");
-		for (int i = 0; i < sb_count(n->stmts); i++) {
-			codegen(ctx, n->stmts[i]);
-		}
-		emit("}\n");
-		break;
-	case ND_FUNC:
-		for (int i = 0; i < sb_count(n->stmts); i++) {
-			codegen(ctx, n->stmts[i]);
-		}
-		break;
-	case ND_DECL:
-		codegen(ctx, n->rhs);
+    switch (n->kind) {
+    case ND_FILE:
+        emit("export function w $main() {\n");
+        emit("@start\n");
+        for (int i = 0; i < sb_count(n->stmts); i++) {
+            codegen(ctx, n->stmts[i]);
+        }
+        emit("}\n");
+        break;
+    case ND_FUNC:
+        for (int i = 0; i < sb_count(n->stmts); i++) {
+            codegen(ctx, n->stmts[i]);
+        }
+        break;
+    case ND_DECL:
+        codegen(ctx, n->rhs);
 
-		tokenstr(ctx->src->src, n->tok, buf, sizeof(buf));
-		id = arrpop(ctx->valstack.stack);
-		emit("	%%%s =w add 0, %%_%d\n", buf, id);
-		break;
-	default:
-		if (ND_START_EXPR < n->kind && n->kind < ND_END_EXPR) {
-			codegen_expr(ctx, n);
-		} else if (ND_START_STMT < n->kind && n->kind < ND_END_STMT) {
-			codegen_stmt(ctx, n);
-		}
-		break;
-	}
+        tokenstr(ctx->src->src, n->tok, buf, sizeof(buf));
+        id = arrpop(ctx->valstack.stack);
+        emit("    %%%s =w add 0, %%_%d\n", buf, id);
+        break;
+    default:
+        if (ND_START_EXPR < n->kind && n->kind < ND_END_EXPR) {
+            codegen_expr(ctx, n);
+        } else if (ND_START_STMT < n->kind && n->kind < ND_END_STMT) {
+            codegen_stmt(ctx, n);
+        }
+        break;
+    }
 }
--- ruse.h
+++ ruse.h
@@ -7,78 +7,78 @@
 #define MAXTOKLEN 64
 
 struct Source {
-	char filename[256]; // source filename
-	size_t *line_offs;  // byte offsets of '\n's
-	char *src;	    // source text
-	long srclen;	    // length of src excluding \0
+    char filename[256]; // source filename
+    size_t *line_offs;  // byte offsets of '\n's
+    char *src;          // source text
+    long srclen;        // length of src excluding \0
 };
 
 struct SrcRange {
-	size_t start; // start position in the source
-	size_t end;   // end position in the source
+    size_t start; // start position in the source
+    size_t end;   // end position in the source
 };
 
 // Represents a unique location (file:line:col) in the source.
 struct SrcLoc {
-	const char *filename;
-	int line;
-	int col;
+    const char *filename;
+    int line;
+    int col;
 };
 
 enum TokenType {
-	TOK_EOF = '\0',
-	TOK_NEWLINE = '\n',
-	TOK_LPAREN = '(',
-	TOK_RPAREN = ')',
-	TOK_DOT = '.',
-	TOK_COLON = ':',
-	TOK_EQUAL = '=',
-	TOK_PLUS = '+',
-	TOK_MINUS = '-',
-	TOK_STAR = '*',
-	TOK_SLASH = '/',
-	TOK_SEMICOLON = ';',
-	TOK_QUOTE = '\'',
-	TOK_ASCII = 256,
+    TOK_EOF = '\0',
+    TOK_NEWLINE = '\n',
+    TOK_LPAREN = '(',
+    TOK_RPAREN = ')',
+    TOK_DOT = '.',
+    TOK_COLON = ':',
+    TOK_EQUAL = '=',
+    TOK_PLUS = '+',
+    TOK_MINUS = '-',
+    TOK_STAR = '*',
+    TOK_SLASH = '/',
+    TOK_SEMICOLON = ';',
+    TOK_QUOTE = '\'',
+    TOK_ASCII = 256,
 
-	TOK_NUM,
-	TOK_IDENT,
-	TOK_STRING,
-	TOK_COMMENT,
+    TOK_NUM,
+    TOK_IDENT,
+    TOK_STRING,
+    TOK_COMMENT,
 
-	TOK_KEYWORDS,
-	TOK_VAR,
-	TOK_PROC,
-	TOK_RETURN,
-	TOK_END,
+    TOK_KEYWORDS,
+    TOK_VAR,
+    TOK_PROC,
+    TOK_RETURN,
+    TOK_END,
 
-	TOK_ERR,
-	NUM_TOKENTYPES
+    TOK_ERR,
+    NUM_TOKENTYPES
 };
 
 extern char *token_names[NUM_TOKENTYPES];
 
 struct token_map {
-	const char *text;
-	enum TokenType type;
+    const char *text;
+    enum TokenType type;
 };
 
 // Making Tokens store source ranges instead of string memory blocks makes
 // passing them around easy.
 struct Token {
-	enum TokenType type;
-	struct SrcRange range;
+    enum TokenType type;
+    struct SrcRange range;
 };
 
 // Lexer state.
 struct Lexer {
-	struct Source src;  // program source
-	struct Token tok;   // currently lexed token
-	char ch;	    // lookahead character
-	long off;	    // lookahead character offset
-	long rd_off;	    // next read character offset
-	long line_off;	    // current line offset
-	long start;	    // start position of `tok`
+    struct Source src; // program source
+    struct Token tok;  // currently lexed token
+    char ch;           // lookahead character
+    long off;          // lookahead character offset
+    long rd_off;       // next read character offset
+    long line_off;     // current line offset
+    long start;        // start position of `tok`
 };
 
 int lexer_from_file(struct Lexer *l, const char *filename);
@@ -93,41 +93,41 @@ char *tokentypestr(enum TokenType t, char *buf, size_t blen);
 void tokenprint(const char *src, const struct Token tok);
 
 enum NodeKind {
-	ND_FILE,
-	ND_FUNC,
-	ND_START_EXPR,
-	ND_LITERAL,
-	ND_IDEXPR,
-	ND_BINEXPR,
-	ND_END_EXPR,
-	ND_DECL,
-	ND_START_STMT,
-	ND_EXPRSTMT,
-	ND_ASSIGN,
-	ND_RETURN,
-	ND_END_STMT,
+    ND_FILE,
+    ND_FUNC,
+    ND_START_EXPR,
+    ND_LITERAL,
+    ND_IDEXPR,
+    ND_BINEXPR,
+    ND_END_EXPR,
+    ND_DECL,
+    ND_START_STMT,
+    ND_EXPRSTMT,
+    ND_ASSIGN,
+    ND_RETURN,
+    ND_END_STMT,
 };
 
 // AST node.
 struct Node {
-	enum NodeKind kind;
-	struct Token tok;
-	long num;
-	struct Decl *decl;
-	struct Node **stmts;
-	struct Node *lhs;
-	struct Node *rhs;
-	// functions
-	struct Node *rettypeexpr;
-	// children for body stmts
+    enum NodeKind kind;
+    struct Token tok;
+    long num;
+    struct Decl *decl;
+    struct Node **stmts;
+    struct Node *lhs;
+    struct Node *rhs;
+    // functions
+    struct Node *rettypeexpr;
+    // children for body stmts
 };
 
 // Source text = ['tok' 'lookahead...' ...unlexed...]
 struct Parser {
-	struct Lexer l;		  // lexer driven by this parser
-	struct Token tok;	  // current token
-	struct Token *lookahead;  // lookahead tokens
-	struct Node **nodeptrbuf; // pointers to the allocated nodes
+    struct Lexer l;           // lexer driven by this parser
+    struct Token tok;         // current token
+    struct Token *lookahead;  // lookahead tokens
+    struct Node **nodeptrbuf; // pointers to the allocated nodes
 };
 
 void parser_from_file(struct Parser *p, const char *filename);
@@ -136,32 +136,32 @@ void parser_cleanup(struct Parser *p);
 struct Node *parse(struct Parser *p);
 
 enum DeclKind {
-	DCL_NUM,
+    DCL_NUM,
 };
 
 struct Decl {
-	enum DeclKind kind;
-	struct Token name;
-	double num;
+    enum DeclKind kind;
+    struct Token name;
+    double num;
 };
 
 struct Map {
-	struct Token name;
-	struct Decl *decl;
+    struct Token name;
+    struct Decl *decl;
 };
 
 struct Scope {
-	struct Map *tab;
-	struct Scope *outer;
+    struct Map *tab;
+    struct Scope *outer;
 };
 
 struct Context {
-	struct Source *src;
-	struct Scope *scope;
-	struct Valstack {
-		int curr_id; // next id to be pushed to valstack
-		int *stack;  // stack of the id of expression results
-	} valstack;
+    struct Source *src;
+    struct Scope *scope;
+    struct Valstack {
+        int curr_id; // next id to be pushed to valstack
+        int *stack;  // stack of the id of expression results
+    } valstack;
 };
 
 void context_init(struct Context *ctx, struct Source *src);
