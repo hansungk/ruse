f504efde6c7d18268870507e2a4e4caab16339ba
Hansung Kim <stephen422@gmail.com>
2021-12-24 22:36:18 -0800

    test.sh: cleanup temp files

--- test.sh
+++ test.sh
@@ -4,19 +4,31 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 RS="\033[0m"
 
+outputfile=/tmp/ruse.out
+outputlinefile=/tmp/ruse.out.lines
+truthlinefile=/tmp/ruse.truth.lines
+
+_cleanup() {
+    rm -f $outputfile
+    rm -f $outputlinefile
+    rm -f $truthlinefile
+}
+
 _pass() {
+    _cleanup
     echo "${GREEN}PASS${RS} $1"
     exit 0
 }
 
 _fail() {
+    _cleanup
     echo "${RED}FAIL${RS} $1"
     exit 1
 }
 
 _errordiff() {
-    grep "error:" /tmp/ruse.out | cut -d':' -f2 >/tmp/ruse.out.lines
-    grep -n "// error" $1 | cut -d':' -f1 >/tmp/ruse.truth.lines
+    grep "error:" /tmp/ruse.out | cut -d':' -f2 >$outputlinefile
+    grep -n "// error" $1 | cut -d':' -f1 >$truthlinefile
     diff /tmp/ruse.out.lines /tmp/ruse.truth.lines || _fail $1
     _pass $1
 }
@@ -30,7 +42,7 @@ test() {
         ret=$(echo ${header} | awk '{ print $3 }')
     fi
 
-    ./ruse $1 >/tmp/ruse.out 2>&1
+    ./ruse $1 >$outputfile 2>&1
 
     if [ $? -ne $ret ]
     then
