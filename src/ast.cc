fdc95ce2eb06b03b79c1c3b619439f0db1d009bd
Hansung Kim <stephen422@gmail.com>
2022-03-30 03:38:04 -0700

    Return placeholder text from AstNode::text()

--- src/ast.cc
+++ src/ast.cc
@@ -10,6 +10,8 @@ namespace cmp {
 int AstNode::indent = 0;
 
 std::string_view AstNode::text(const Sema &sema) {
+    return "text() placeholder";
+    assert(!"wontfix: AstNodes no longer use text() to get text data");
     return {sema.source.buf.data() + pos, endpos - pos};
 }
 
